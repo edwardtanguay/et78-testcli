b547aded7148d7a8a8949faa110bae2dd210d10a
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-17 16:15:39 +0200

    day finished

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ const siteElem = document.querySelector('#app')
 if (siteElem) {
   siteElem.innerHTML = `
 <h1 class="text-orange-300 text-4xl">CLI Test</h1>
+<h2>sass</h2>
+<button class="btn-primary">test button</button>
 `;
 } else {
   alert('sorry')
