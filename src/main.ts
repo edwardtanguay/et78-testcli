b7b842691a530c5fb7e55de70d885af904ba33e9
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-21 14:09:07 +0200

    testing in output works

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,31 @@ import './style.scss';
 const siteElem = document.querySelector('#app')
 
 if (siteElem) {
-  siteElem.innerHTML = /*html*/ `
-  <form>
-    <input class="message"/>
-    <button class="btn-primary">Post</button>
-  </form>
+	siteElem.innerHTML = /*html*/ `
+	<form>
+		<input class="message"/>
+		<button class="btnMain btn-primary">Post</button>
+	</form>
 
-  <div>
+	<div>
 Output: [<span class="output"></span>]
-  </div>
+	</div>
 `;
 
-  const _outputElem = document.querySelector('.output');
-  if (_outputElem) {
-    const outputElem = _outputElem;
-    
-    outputElem.innerHTML = 'nnn';
-  }
+	const _outputElem = document.querySelector<HTMLSpanElement>('.output');
+	const _btnMainElem = document.querySelector('.btnMain');
+
+	if (_outputElem && _btnMainElem) {
+		const outputElem = _outputElem;
+		const btnMainElem = _btnMainElem;
+
+		btnMainElem.addEventListener('click', (e:Event) => {
+			e.preventDefault();
+			outputElem.innerText = 'testing';
+		});
+	}
 
 
 } else {
-  alert('sorry')
+	alert('sorry')
 }
