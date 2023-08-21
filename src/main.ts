8f7c3bb3564524a16950135900b2947f4affbec0
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-21 14:32:33 +0200

    localstorage finished

--- src/main.ts
+++ src/main.ts
@@ -20,18 +20,26 @@ Output: [<span class="output">${localStorageMessage}</span>]
 	const _outputElem = document.querySelector<HTMLSpanElement>('.output');
 	const _btnPostElem = document.querySelector<HTMLButtonElement>('.btnPost');
 	const _messageElem = document.querySelector<HTMLInputElement>('.message');
+	const _btnClearElem = document.querySelector<HTMLButtonElement>('.btnClear');
 
-	if (_outputElem && _btnPostElem && _messageElem) {
+	if (_outputElem && _btnPostElem && _messageElem && _btnClearElem) {
 		const outputElem = _outputElem;
 		const btnPostElem = _btnPostElem;
 		const messageElem = _messageElem;
+		const btnClearElem = _btnClearElem;
 
-		btnPostElem.addEventListener('click', (e:Event) => {
+		btnPostElem.addEventListener('click', (e: Event) => {
 			e.preventDefault();
 			const message = messageElem.value;
 			outputElem.innerText = message;
 			messageElem.value = '';
 			localStorage.setItem('message', message);
+		});
+
+		btnClearElem.addEventListener('click', (e: Event) => {
+			e.preventDefault();
+			outputElem.innerText = '';
+			localStorage.setItem('message', '');
 		});
 	}
 
