460d150d15914b241b8bf8c47848f1f402209ce1
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-24 13:34:39 +0200

    reading in pageName

--- backend/cp.js
+++ backend/cp.js
@@ -1,3 +1,10 @@
 import * as tools from './tools.js';
 
-console.log('cli test: ' + tools.getTestMessage());
+const pageName = process.argv[2];
+
+if (!pageName) {
+	console.log('Please run script with a page name, e.g. "npm run cp Reports".')
+	process.exit();
+} else {
+	console.log(`Creating page ${pageName}...`)
+}
